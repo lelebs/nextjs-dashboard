200627042cdb58e8a0fbf0395465713d26713bd5
Leandro Gabatel <leandrogabatel@Leandros-MacBook-Pro.local>
2025-02-13 15:58:10 -0300

    fix lint issues

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
